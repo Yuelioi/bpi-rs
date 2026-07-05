dbbb4d3fb75c7c46c11edafd2891b5613102a1a7
月离 <yuelioi1210@gmail.com>
2026-07-06 06:07:00 +0800

    fix(account): 导出账号配置类型

--- src/session/mod.rs
+++ src/session/mod.rs
@@ -1,6 +1,4 @@
 pub mod account;
 pub mod cookie;
 
-pub use account::Account;
-#[cfg(any(test, debug_assertions))]
-pub use account::TestAccountProfile;
+pub use account::{Account, AccountProfile};
